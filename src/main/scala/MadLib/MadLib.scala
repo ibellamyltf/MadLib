54b7c0837136ceea6e89a92e9084b7e5740f9498
Ian Bellamy <bellamy.ian.m@gmail.com>
2015-06-13 23:16:29 -0600

    additional tests and almost there

--- src/main/scala/MadLib/MadLib.scala
+++ src/main/scala/MadLib/MadLib.scala
@@ -7,12 +7,11 @@ package MadLib
 import org.jsoup.Jsoup
 import org.jsoup.select.Elements
 import scala.collection.JavaConversions._
-
-
-import scala.sys.process._
+//
+// import scala.sys.process._
 import scala.language.postfixOps
-import java.net.URL
-
+// import java.net.URL
+//
 import epic.preprocess._
 import epic.models.PosTagSelector
 
@@ -48,7 +47,6 @@ object MadLib {
   // grabs the links from each row of Missed Connections
 
   val elems = findElements(baseURL+"search/mis", ".i")
-  // val titles = findElements(baseURL+"search/mis", ???)
   val links = listURLs(elems)
   val docs = for (href <- links) yield findElements(baseURL+href, "#postingbody")
 
@@ -60,18 +58,31 @@ object MadLib {
   /*
   Text processing guidelines:
   Each text blob has 300 or more characters; that's enough to make a MadLib!
-  Take a proportion as a guideline, where out of x nouns, you replace n/m of them
 
   Testing: just make one that gets the parts of speech and case matches it back in
-
   see https://github.com/dlwh/epic
    */
+
+  // much like the tests, please check those out.
+
   val sentenceSplitter = MLSentenceSegmenter.bundled().get
   val tokenizer = new epic.preprocess.TreebankTokenizer()
+  val sentences: IndexedSeq[IndexedSeq[String]] = sentenceSplitter(cleaned(0)).map(tokenizer).toIndexedSeq
+  val tagger = PosTagSelector.loadTagger("en").get
+  val tags = for {
+    sentence <- sentences
+  } yield tagger.bestSequence(sentence)
+
+  def parseText(text: IndexedSeq[String]) ={
+
+
+  }
+
   // will have to do this for each sentence!
   //val sentences: IndexedSeq[IndexedSeq[String]] = sentenceSplitter(text).map(tokenizer).toIndexedSeq
 
-  val tagger = PosTagSelector.loadTagger("en").get
+
+
 
 
 
--- src/test/scala/TestMadLib.scala
+++ src/test/scala/TestMadLib.scala
@@ -4,9 +4,18 @@ import MadLib._
 
 class TestMadLib extends FlatSpec with Matchers {
 
-  "Craigslist" should "deploy numerours asynchronous workers to grab pages" in {
+  "MadLib" should "grab pages and preproceess" in {
 
+    val baseURL = "http://denver.craigslist.org/"
+    // remember .content
+    val elements = MadLib.findElements(baseURL+"search/mis", ".i")
+    val urls = MadLib.listURLs(elements)
+    val docs = for (href <- urls) yield MadLib.findElements(baseURL+href, "#postingbody")
+    val cleaned = MadLib.cleanElems(docs)
 
+    println(cleaned(0))
+    println(MadLib.sentences)
+    println(MadLib.tags(0).render)
 
   }
 }
